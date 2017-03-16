f9c3859889caa23d88783211d41d7b85e874a9dd
张强 <494000616@qq.com>
2017-03-16 14:19:35 +0800

    第一次提交

---
+++ platforms/ios/Share/ShareViewController.h
@@ -0,0 +1,14 @@
+//
+//  ShareViewController.h
+//  Share
+//
+//  Created by 张强 on 2017/3/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import <Social/Social.h>
+
+@interface ShareViewController : SLComposeServiceViewController
+
+@end
---
+++ platforms/ios/Share/ShareViewController.m
@@ -0,0 +1,64 @@
+//
+//  ShareViewController.m
+//  Share
+//
+//  Created by 张强 on 2017/3/16.
+//
+//
+
+#import "ShareViewController.h"
+
+@interface ShareViewController ()
+
+@end
+
+@implementation ShareViewController
+
+- (BOOL)isContentValid {
+    // Do validation of contentText and/or NSExtensionContext attachments here
+    return YES;
+}
+
+- (void)didSelectPost {
+    // This is called after the user selects Post. Do the upload of contentText and/or NSExtensionContext attachments.
+    __block BOOL hasExistsUrl = NO;
+    [self.extensionContext.inputItems enumerateObjectsUsingBlock:^(NSExtensionItem * _Nonnull extItem, NSUInteger idx, BOOL * _Nonnull stop) {
+        
+        [extItem.attachments enumerateObjectsUsingBlock:^(NSItemProvider * _Nonnull itemProvider, NSUInteger idx, BOOL * _Nonnull stop) {
+            
+            if ([itemProvider hasItemConformingToTypeIdentifier:@"public.url"])
+            {
+                [itemProvider loadItemForTypeIdentifier:@"public.url"
+                                                options:nil
+                                      completionHandler:^(id<NSSecureCoding>  _Nullable item, NSError * _Null_unspecified error) {
+                                          
+                                          if ([(NSObject *)item isKindOfClass:[NSURL class]])
+                                          {
+                                              NSLog(@"分享的URL = %@", item);
+                                          }
+                                          
+                                      }];
+                
+                hasExistsUrl = YES;
+                *stop = YES;
+            }
+            
+        }];
+        
+        if (hasExistsUrl)
+        {
+            *stop = YES;
+        }
+        
+    }];
+
+    // Inform the host that we're done, so it un-blocks its UI. Note: Alternatively you could call super's -didSelectPost, which will similarly complete the extension context.
+    [self.extensionContext completeRequestReturningItems:@[] completionHandler:nil];
+}
+
+- (NSArray *)configurationItems {
+    // To add configuration options via table cells at the bottom of the sheet, return an array of SLComposeSheetConfigurationItem here.
+    return @[];
+}
+
+@end
